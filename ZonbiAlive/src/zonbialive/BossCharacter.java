43625e869a533b233159e73583e936728afd938d
yurukyara <40050780+yurukyara@users.noreply.github.com>
2018-06-08 14:26:32 +0900

    room

---
+++ ZonbiAlive/src/zonbialive/BossCharacter.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package zonbialive;
+
+/**
+ *
+ * @author C0116125
+ */
+public class BossCharacter {
+    private int bossHitPoint;
+    private int bossAttack;
+
+    public BossCharacter(int bossHitPoint, int bossAttack) {
+        this.bossHitPoint = bossHitPoint;
+    }
+
+    public int getBossHitPoint() {
+        return bossHitPoint;
+    }
+
+    public int getBossAttack() {//ここで攻撃力を決定しダメージを返す
+        return bossAttack;
+    }
+    
+    
+    
+
+}
