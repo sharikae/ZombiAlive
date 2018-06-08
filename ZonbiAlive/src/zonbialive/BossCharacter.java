21a1dadc3e0b611847f34a28ae6efaedb00c7374
yurukyara <40050780+yurukyara@users.noreply.github.com>
2018-06-08 15:56:50 +0900

    完成系・・・？

--- ZonbiAlive/src/zonbialive/BossCharacter.java
+++ ZonbiAlive/src/zonbialive/BossCharacter.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package zonbialive;
+//package zonbialive;
 
 /**
  *
@@ -13,15 +13,28 @@ public class BossCharacter {
     private int bossHitPoint;
     private int bossAttack;
 
-    public BossCharacter(int bossHitPoint, int bossAttack) {
-        this.bossHitPoint = bossHitPoint;
+    public BossCharacter(){//初期化
+        this.bossHitPoint=35;
     }
 
+    //ボスに対するダメージ
+    public void damagedHitPoint(int damaged) {
+        this.bossHitPoint -= damaged;
+        if(this.bossHitPoint<=0){
+            
+        }
+    }
+    //現在HPを指定
     public int getBossHitPoint() {
+        //マイナス数値を出さない為の処理。問題がある場合は削除
+        if(this.bossHitPoint<0){
+            this.bossHitPoint=0;
+        };
         return bossHitPoint;
     }
 
     public int getBossAttack() {//ここで攻撃力を決定しダメージを返す
+        this.bossAttack=(int)(1+Math.random()*5);//1D5のダメージ
         return bossAttack;
     }
     
