6d9dc149eec616e34c23497c610eb0810b1bd320
yurukyara <40050780+yurukyara@users.noreply.github.com>
2018-06-08 14:21:47 +0900

    BossCharacter

--- objects/BossCharacter.java
+++ objects/BossCharacter.java
@@ -10,9 +10,17 @@ package zonbialive;
  * @author C0116125
  */
 public class BossCharacter {
-    private int bossHitPoint=50;
+    private int bossHitPoint;
     private int bossAttack;
-    
-    
+    public BossCharacter(int bossHitPoint, int bossAttack) {
+        this.bossHitPoint = bossHitPoint;
+    }
 
+    public int getBossHitPoint() {
+        return bossHitPoint;
+    }
+
+    public int getBossAttack() {//‚±‚±‚ÅUŒ‚—Í‚ğŒˆ’è‚µƒ_ƒ[ƒW‚ğ•Ô‚·
+        return bossAttack;
+    }
 }
