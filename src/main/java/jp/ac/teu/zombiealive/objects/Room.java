4fb4627fc956dc95ade03f7b436b19243e85cecd
chibaop <40032262+chibaop@users.noreply.github.com>
2018-06-15 17:57:20 +0900

    2次元配列で管理

--- src/main/java/jp/ac/teu/zombiealive/objects/Room.java
+++ src/main/java/jp/ac/teu/zombiealive/objects/Room.java
@@ -4,162 +4,49 @@ package jp.ac.teu.zombiealive.objects;
  * @author C0116086
  */
 public class Room {
-    boolean player;
-    boolean daughter;
-    boolean zombie;
-    boolean key;
-    boolean item;
-    boolean boss;
-    boolean north;
-    boolean west;
-    boolean south;
-    boolean east;
-    int zombieNum;
-
-    public Room(int roomNo) {
-        zombieNum = 0;
-    }
+    int[][] room
+            = {{0, 0, 1, 2, 1, 0, 0, 3, 2, 0, 2, 0, 0, 1, 1, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}};
 
     public int getZombieNum(int roomNo) {
-        switch (roomNo) {
-            case 2:
-            case 4:
-            case 13:
-            case 14:
-            case 16:
-            case 17:
-            case 19:
-            case 23:
-                zombieNum = 1;
-                break;
-            case 3:
-            case 8:
-            case 10:
-                zombieNum = 2;
-                break;
-            case 7:
-                zombieNum = 3;
-                key = true;
-                break;
-            case 5:
-            case 12:
-            case 15:
-                item = true;
-                zombieNum = 0;
-                break;
-            case 6:
-                boss = true;
-                zombieNum = 0;
-                break;
-            default:
-                zombieNum = 0;
-        }
-        return zombieNum;
+        return room[0][roomNo];
     }
 
-    public void zombieLocator() {
-
-        if (zombie = true) {
-            zombieNum += 1;
-            System.out.println(zombieNum + " Zombies found!");
+    public boolean getItem(int roomNo) {
+        if (room[1][roomNo] != 0) {
+            return true;
         } else {
-            System.out.println("No Zombies.");
+            return false;
         }
     }
 
-    public void Inventory() {
-        if (key = true) {
-            System.out.println("You have a key.");
+    public boolean getKey(int roomNo) {
+        if (room[2][roomNo] != 0) {
+            return true;
         } else {
-            System.out.println("You don't have a key.");
-        }
-
-        if (item = true) {
-            System.out.println("You have an Item.");
-        } else {
-            System.out.println("You don't have an Item.");
+            return false;
         }
     }
 
-    public void Daughter() {
-        if (daughter = true) {
-            System.out.println("a daughter is here.");
+    public boolean getBoss(int roomNo) {
+        if (room[3][roomNo] != 0) {
+            return true;
         } else {
-            System.out.println("No daughter here.");
+            return false;
         }
     }
-    
-    public boolean[] nextMove(int roomNo) {        
-        switch (roomNo) {
-            case 1:
-            case 18:
-                east = true;
-                south = true;
-                break;
-            case 2:
-            case 3:
-            case 5:
-            case 8:
-            case 19:
-            case 22:
-                west = true;
-                east = true;
-                break;
-            case 4:
-                west = true;
-                east = true;
-                south = true;
-                break;
-            case 7:
-            case 11:
-            case 14:
-            case 16:
-                east = true;
-                break;
-            case 9:
-            case 15:
-            case 17:
-            case 25:
-                west = true;
-                break;
-            case 10:
-            case 20:
-                north = true;
-                south = true;
-                break;
-            case 13:
-                north = true;
-                break;
-            case 21:
-            case 24:
-                north = true;
-                east = true;
-                break;
-            case 23:
-                north = true;
-                west = true;
-                break;
-            default:
-                break;
-        }
-        
-        boolean[] nextMove = new boolean[4];
-        if (north == true) {
-            nextMove[0] = true;
-            //Move to Northの処理
-        }
-        if (east == true) {
-            nextMove[1] = true;
-            //Movie to Eastの処理
-        }
-        if (south == true) {
-            nextMove[2] = true;
-            //Move to Southの処理
-        }
-        if (west == true) {
-            nextMove[3] = true;
-            //Move to Westの処理
-        }
-        return nextMove;
+
+    public void setZombieNum(int roomNo) {
+        room[0][roomNo] = 0;
+    }
+
+    public void setItem(int roomNo) {
+        room[1][roomNo] = 0;
+    }
+
+    public void setKey(int roomNo) {
+        room[2][roomNo] = 0;
     }
 }
