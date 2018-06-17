a141d99dc6ba920c1936dfd0fa6a15ee5c5b4861
chibaop <40032262+chibaop@users.noreply.github.com>
2018-06-18 01:23:15 +0900

    Timerクラスの実装
    簡単にCalendar型での判断を追加した。

--- src/main/java/jp/ac/teu/zombiealive/util/Timer.java
+++ src/main/java/jp/ac/teu/zombiealive/util/Timer.java
@@ -1,4 +1,18 @@
 package jp.ac.teu.zombiealive.util;
 
+import java.util.Calendar;
+
 public class Timer {
+
+    Calendar timeUp = Calendar.getInstance();
+
+    public Timer() {
+        timeUp.add(Calendar.MINUTE, 15);//Timerクラス初期化の15分後にセット
+    }
+
+    public boolean getTimer() {//時間がたったかどうかの判断
+        Calendar start = Calendar.getInstance();
+        boolean time = start.before(timeUp);
+        return time;
+    }
 }
