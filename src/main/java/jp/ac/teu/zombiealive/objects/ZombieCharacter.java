165b72a37e99e0a4a0e39bdaca8202fd11e47d03
yurukyara <40050780+yurukyara@users.noreply.github.com>
2018-07-06 14:08:26 +0900

    kill数の記載

--- src/main/java/jp/ac/teu/zombiealive/objects/ZombieCharacter.java
+++ src/main/java/jp/ac/teu/zombiealive/objects/ZombieCharacter.java
@@ -2,14 +2,17 @@ package jp.ac.teu.zombiealive.objects;
 
 /**
  * @author C0116178
+ * @author c0116125
  */
 
 public class ZombieCharacter {
 
+    private static int kill=0;
     private int zombieHitPoint;
 
     public ZombieCharacter() {
         zombieHitPoint = 6;
+        kill++;
     }
 
     public int getHp() {                        //ゾンビのHPを返す
@@ -19,4 +22,10 @@ public class ZombieCharacter {
     public void damageToZombie(int damage) {    //ゾンビのHPを減らす
         zombieHitPoint -= damage;
     }
+
+    public static int getKill() {
+        return kill;
+    }
+    
+    
 }
